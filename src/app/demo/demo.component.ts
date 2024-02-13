0d1cbb88fba2c6b3d9c719c94d11f9a7cc9fcbff
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-02-13 17:04:10 +0545

    Learned ngOnChanges lifecycle hook.

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,18 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent {
+export class DemoComponent implements OnChanges{
   
   title: string = 'Demo Component';
-  @Input() message: string = '';
+  @Input() message?: string;
 
   constructor(){
     console.log('DemoComponent constructor called');
-    console.log('this.title');
-    console.log('this.message');
+    console.log(this.title);
+    console.log(this.message);
   };
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('On changes hook called'); 
+    console.log(changes);
+
+  }
 }
