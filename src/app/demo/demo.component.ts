4215dd2935bf11821a118d8b0d917577dc6d6896
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-02-15 17:29:53 +0545

    Learned ngAfterContentInit lifecycle hook.

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnChanges, SimpleChanges,OnInit, ElementRef, ViewChild,DoCheck} from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges,OnInit, ElementRef, ViewChild,DoCheck, AfterContentInit, ContentChild} from '@angular/core';
 
 @Component({
   selector: 'demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges, OnInit, DoCheck{
+export class DemoComponent implements OnChanges, OnInit, DoCheck,AfterContentInit{
   
   title: string = 'Demo Component';
   @Input() message?: string;
   @ViewChild('temp') tempPara?: ElementRef;
-
+  @ContentChild('temp') paraContent?: ElementRef;
+ 
   constructor(){
     console.log('DemoComponent constructor called');
     // console.log(this.title);
@@ -29,6 +30,12 @@ export class DemoComponent implements OnChanges, OnInit, DoCheck{
   }
   ngDoCheck() {
     console.log('ngDoCheck hook called'); 
+    console.log("In a ngDoCheck", this.paraContent);
+
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit hook called');
+    console.log("In a ngAfterContentInit", this.paraContent?.nativeElement);
 
   }
 }
